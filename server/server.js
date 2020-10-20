a8f31ab23e9337a005451c184e8bc2a8c3aa9a33
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-19 22:18:36 -0500

    sketching out app.post and leaving TODO comments for later

--- server/server.js
+++ server/server.js
@@ -12,12 +12,25 @@ const coffeeRecipe = [];
 // TODO  - ROUTES BELOW HERE
 
 //  GET get results of calculations ---> client side
-
 app.get('/coffee', (req, res) => {
   res.send(coffeeRecipe);
 });
 
 // POST - for receiving coffee inputs from DOM , calculations
+/*
+
+req.body{
+  coffeeWaterRatio: number (use parseInt)
+  desiredVolume: number (use parseInt)
+}
+*/
+app.post('/coffee', (req, res) => {
+  const coffee = parseInt(req.body.coffeeWaterRatio);
+  const volume = parseInt(req.body.desiredVolume);
+  // coffee should come in from client side as a whole number.. multiplied by .001 to adjust for 'grams to Liter'
+  console.log(coffee, volume);
+  res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
   console.log('Listening on PORT: ', PORT);
