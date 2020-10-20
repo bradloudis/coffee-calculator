482ce58a73cbf332516cd7babd8c151a9581ea5c
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-19 21:55:31 -0500

    app.get sends coffeeRecipe to the client side

--- server/server.js
+++ server/server.js
@@ -7,9 +7,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+const coffeeRecipe = [];
+
 // TODO  - ROUTES BELOW HERE
 
 //  GET get results of calculations ---> client side
+
+app.get('/coffee', (req, res) => {
+  res.send(coffeeRecipe);
+});
 
 // POST - for receiving coffee inputs from DOM , calculations
 
