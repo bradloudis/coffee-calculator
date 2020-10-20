3b3c8be1c7cfef5886834ced59baf62bedaa3120
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-19 21:50:05 -0500

    upgrades to server.js to stop using bodyParser since it is included in express

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 // TODO  - ROUTES BELOW HERE
 
-// TODO - GET get results of calculations ---> client side
+//  GET get results of calculations ---> client side
 
 // POST - for receiving coffee inputs from DOM , calculations
 
-app.use(express.static('public'));
 app.listen(PORT, () => {
   console.log('Listening on PORT: ', PORT);
 });
