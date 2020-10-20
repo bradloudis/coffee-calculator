8c74e80f3b78453ea2988ace5e6039dce0f77c54
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-19 22:22:51 -0500

    accidental keystroke or something in server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 const coffeeRecipe = [];
 
 // TODO  - ROUTES BELOW HERE
+// TODO - should be able to calculate weight of coffee needed for brew
+//                                OR
+//        should be able to calculate total water needed for brew (think weird amount of beans at end of bag)
 
 //  GET get results of calculations ---> client side
 app.get('/coffee', (req, res) => {
